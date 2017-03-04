0fb67ad5fc05c01fbd0ee7dd79649d885952ba77
mbaltman <mbaltman@cps.edu>
2017-03-04 09:18:45 -0600

    Add files via upload

--- Steam Works/src/org/usfirst/frc/team4645/robot/commands/PlaceGearCommand.java
+++ Steam Works/src/org/usfirst/frc/team4645/robot/commands/PlaceGearCommand.java
@@ -41,7 +41,7 @@ public class PlaceGearCommand extends CommandGroup
         
     	//updates vision values and moves in Y, pushes gear
     	distanceInformation=(Robot.visionSubsystem.returnGearInformation());
-     	addSequential(new MoveToY(RobotMap.GEAR_DISTANCEY+distanceInformation[1]));
+     	addSequential(new MoveToY(RobotMap.GEAR_DISTANCE+distanceInformation[1]));
      
      	addSequential(new PushGearCommand());
      	
